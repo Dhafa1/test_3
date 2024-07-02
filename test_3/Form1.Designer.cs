88f624960d78c9492f2b4197218467d1e96ba852
Lenovo <Lenovo@LAPTOP-DF5HQVVQ>
2024-07-02 11:46:20 +0700

    Add project files.

---
+++ test_3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace testList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_list = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btn_list
+            // 
+            this.btn_list.Location = new System.Drawing.Point(340, 361);
+            this.btn_list.Name = "btn_list";
+            this.btn_list.Size = new System.Drawing.Size(105, 34);
+            this.btn_list.TabIndex = 1;
+            this.btn_list.Text = "List Hero";
+            this.btn_list.UseVisualStyleBackColor = true;
+            this.btn_list.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(27, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(298, 411);
+            this.treeView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btn_list);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_list;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
